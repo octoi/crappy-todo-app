a84cd1fac0b9b5baca10e3e835b726081c96e0f5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-18 23:01:05 +0530

    states, useffect and input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("")
+
+    useEffect(() => {
+        if (!localStorage) return;
+
+        let todosFromStorage = localStorage.getItem("todos");
+        if (todosFromStorage) setTodos(todos);
+
+        return () => {
+            localStorage.setItem("todos", todos)
+        }
+    }, [todos]);
+
     return (
         <div className="App">
+            <h1>Todo App</h1>
+            <input
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                placeholder="To do ??"
+            />
         </div>
     );
 }
