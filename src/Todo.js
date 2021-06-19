7b5d275a83b5056ea9cce99750a1611054077d54
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-19 13:28:57 +0530

    cleanup

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,7 @@
 export default function Todo({ todo, deleteTodo, resolveTodo }) {
     return (
         <div className="todo">
-            <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="todo-content">
                 <input onChange={() => resolveTodo(todo.id)} className="checkbox" type="checkbox" checked={todo.isDone} />
                 <p className={todo.isDone ? "completed-task" : ""}>{todo.title}</p>
             </div>
