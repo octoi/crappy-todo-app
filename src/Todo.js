e59aac5994edafd25cef3d6dc8106ab6ce66c09a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-19 13:36:05 +0530

    ask to delete todo

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,21 @@
 export default function Todo({ todo, deleteTodo, resolveTodo }) {
+
+    const askToDeleteTodo = () => {
+        if (window.confirm) {
+            const permission = window.confirm("Are you sure ??")
+            if (!permission) return;
+        }
+
+        deleteTodo(todo.id)
+    }
+
     return (
         <div className="todo">
             <div className="todo-content">
                 <input onChange={() => resolveTodo(todo.id)} className="checkbox" type="checkbox" checked={todo.isDone} />
                 <p className={todo.isDone ? "completed-task" : ""}>{todo.title}</p>
             </div>
-            <button onClick={() => deleteTodo(todo.id)}><i className="far fa-trash-alt"></i></button>
+            <button onClick={askToDeleteTodo}><i className="far fa-trash-alt"></i></button>
         </div>
     )
 }
