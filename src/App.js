c569f034d95bd741b13e09d00c6efa0a73b0f90e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-23 09:43:16 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Todo from './Todo';
+import Todo from './components/Todo';
 
 function App() {
     const [todo, setTodo] = useState("");
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./pwa/serviceWorkerRegistration";
 
 ReactDOM.render(
     <React.StrictMode>
