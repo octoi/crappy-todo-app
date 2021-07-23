4649cf37b9cf0d9aa1e5a53e948d9f9d696c90a0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-23 11:44:58 +0530

    basic drag

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import Todo from './Todo';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 export default function TodoList({ todos, todoHelper, filter }) {
     return (
-        <div style={{ marginTop: "30px" }}>
-            {todos !== "" && todos.map(todo => {
-                if (filter === "done" && !todo.isDone) return null;
-                if (filter === "todo" && todo.isDone) return null;
-                return <Todo
-                    todo={todo}
-                    key={todo.id}
-                    deleteTodo={() => todoHelper.deleteTodo(todos, todo.id)}
-                    resolveTodo={() => todoHelper.resolveTodo(todos, todo.id)}
-                />
-            })}
-        </div>
+        <DragDropContext>
+            <Droppable droppableId="todos">
+                {(provided) => (
+                    <ul style={{ marginTop: "30px" }} {...provided.droppableProps} ref={provided.innerRef}>
+                        {todos !== "" && todos.map((todo, idx) => {
+                            if (filter === "done" && !todo.isDone) return null;
+                            if (filter === "todo" && todo.isDone) return null;
+                            return (
+                                <Draggable key={idx} draggableId={`item-${idx}`} index={idx}>
+                                    {(provided) => <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                                        <Todo
+                                            todo={todo}
+                                            key={todo.id}
+                                            deleteTodo={() => todoHelper.deleteTodo(todos, todo.id)}
+                                            resolveTodo={() => todoHelper.resolveTodo(todos, todo.id)}
+                                        />
+                                    </li>}
+                                </Draggable>
+                            )
+                        })}
+                        {provided.placeholder}
+                    </ul>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
