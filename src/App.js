c04a7a825224150fa727b2afc3b5463d5c28e5b8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-23 10:50:38 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,35 +2,13 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 import TodoInput from './components/TodoInput';
+import TodoHelper from './utils/todoHelper';
 
 function App() {
     const [todos, setTodos] = useState("");
     const [filter, setFilter] = useState("all");
 
-    const addTodo = (todo) => {
-        const newTodo = {
-            title: todo,
-            isDone: false,
-            id: Date.now()
-        };
-
-        setTodos(todos => [...todos, newTodo]);
-    }
-
-    const deleteTodo = (id) => {
-        let newTodos = todos;
-        newTodos = newTodos.filter(todoItem => todoItem.id !== id);
-        setTodos(newTodos)
-    }
-
-    const resolveTodo = (id) => {
-        setTodos(todos.filter(todoItem => {
-            if (todoItem.id === id) {
-                todoItem.isDone = !todoItem.isDone;
-            }
-            return todoItem;
-        }))
-    }
+    const todoHelper = new TodoHelper(setTodos);
 
     useEffect(() => {
         if (!localStorage) return;
@@ -50,7 +28,7 @@ function App() {
     return (
         <div className="App">
             <h1>Todo App</h1>
-            <TodoInput addTodo={addTodo} />
+            <TodoInput addTodo={(todo) => todoHelper.addTodo(todo)} />
             <div className="options">
                 <button className={filter === "all" ? "selected-btn" : ""} onClick={() => setFilter("all")}>All</button>
                 <button className={filter === "done" ? "selected-btn" : ""} onClick={() => setFilter("done")}>Completed</button>
@@ -60,7 +38,12 @@ function App() {
                 {todos !== "" && todos.map(todo => {
                     if (filter === "done" && !todo.isDone) return null;
                     if (filter === "todo" && todo.isDone) return null;
-                    return <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo} resolveTodo={resolveTodo} />
+                    return <Todo
+                        todo={todo}
+                        key={todo.id}
+                        deleteTodo={() => todoHelper.deleteTodo(todos, todo.id)}
+                        resolveTodo={() => todoHelper.resolveTodo(todos, todo.id)}
+                    />
                 })}
             </div>
         </div>
