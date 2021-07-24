92cb599491b59ca8da290b4f13a908c6b352fbbe
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-24 10:05:11 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import * as serviceWorkerRegistration from "./pwa/serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
     <React.StrictMode>
